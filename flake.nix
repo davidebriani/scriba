87b7d72b4f5524840a75559d49e00476e29cd535
Davide Briani <davide@briani.dev>
2025-08-04 17:02:44 +0200

    Add libstdc++ to runtime dependencies

--- flake.nix
+++ flake.nix
@@ -50,6 +50,7 @@
           curl              # HTTP client library
           unzip             # For extracting models
           xdotool           # For xdo library (keyboard simulation)
+          stdenv.cc.cc.lib  # libstdc++
         ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
           # macOS-specific dependencies
           darwin.apple_sdk.frameworks.CoreAudio
