fcb754c521211df664c82f908aa33957df2865ca
Davide Briani <davide@briani.dev>
2025-08-05 02:13:07 +0200

    Use gcc.cc instead of gcc.cc.lib for runtime dependencies

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
           curl              # HTTP client library
           unzip             # For extracting models
           xdotool           # For xdo library (keyboard simulation)
-          gcc.cc.lib        # libstdc++
+          gcc.cc            # GCC compiler collection (includes libstdc++)
         ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
           # macOS-specific dependencies
           darwin.apple_sdk.frameworks.CoreAudio
